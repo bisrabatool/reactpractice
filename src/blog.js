d73057a4a8467cb4470fff92a3552b34a89477ef
Bisra Batool <bisrabatool345@gmail.com>
2024-02-21 21:30:06 -0800

    react blog app

---
+++ src/blog.js
@@ -0,0 +1,14 @@
+import {Col, Container, Row} from 'react-bootstrap'
+import {Header}from 'main.js'
+
+function App(){
+    return(
+        <Container>
+        <Row><Col><Header/></Col></Row>
+        <Row><Col>Blogs</Col></Row>
+        </Container>
+       
+    )
+
+}
+export default header
